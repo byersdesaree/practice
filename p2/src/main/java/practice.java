6fe2c5dd182d3fb14a414e740e7ce7c28ce8b193
Desaree Byers <dariusbrown@Dariuss-MacBook-Pro.local>
2019-07-19 09:43:17 -0500

    concatenates

--- p2/src/main/java/practice.java
+++ p2/src/main/java/practice.java
@@ -2,4 +2,13 @@ public class practice {
     public static void main(String[] args){
         System.out.println("Hello World");
     }
+
+    public void concatenates(String word){
+        System.out.println("Hello World"+ "Goodnight moon");
+    }
+    public void concatenates(String word, String word2){
+        String word="Hello everyone";
+        String word2="How are you";
+        System.out.println(word + word2);
+    }
 }
